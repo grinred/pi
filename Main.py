6196c3bd8755ed6d609a1cbc22d1092be9bbbaf9
Sergey Grinchuk <grinred69@gmail.com>
2025-10-21 11:24:51 +0300

    add function in Main status add servo code

--- Main.py
+++ Main.py
@@ -1,5 +1,18 @@
-import time
+from adafruit_servokit import ServoKit
+from time import sleep
 
-for i in range(0, 3):
-    print("Hi: " + str(i))
-    time.sleep(1)
+kit = ServoKit(channels=16)
+
+
+def servo1():
+    for a in range(0, 180):
+        kit.servo[0].angle = a
+        sleep(0.008)
+
+    for a in range(179, 1, -1):
+        kit.servo[0].angle = a
+        sleep(0.008)
+
+
+while True:
+    servo1()
